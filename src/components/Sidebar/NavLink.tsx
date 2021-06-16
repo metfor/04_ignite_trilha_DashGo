43f8c41bb9643458da78f23c7335c31900d5cf2a
metfor <mateus_frade@hotmail.com>
2021-06-16 11:16:09 -0300

    pt1 4/5

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import {Text,Link as ChakraLink, Icon, LinkProps as ChakraLinkProps} from "@chakra-ui/react"
 import { ElementType } from "react"
-import Link from "next/link"
+
+import { ActiveLink } from "../ActiveLink"
 interface NavLinkProps extends ChakraLinkProps{
     icon:ElementType,
     text:string,
@@ -9,11 +10,11 @@ interface NavLinkProps extends ChakraLinkProps{
 }
 export function NavLink({icon,text,href, ...rest}:NavLinkProps){
     return(
-        <Link href={href} passHref>
+        <ActiveLink href={href} passHref>
             <ChakraLink display="flex" align="center" color="pink.400"{...rest}>
                 <Icon as={icon} fontSize="20"/>
                 <Text ml="4" fontWeight="medium">{text}</Text>
             </ChakraLink>
-        </Link>
+        </ActiveLink>
     )
 }
