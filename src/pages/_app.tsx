fdca08939824357437654fed5b5972deae739747
metfor <mateus_frade@hotmail.com>
2021-06-17 14:43:15 -0300

    pt2 1/2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import{AppProps} from"next/app"
 import{ChakraProvider}from"@chakra-ui/react"
 import{theme}from "../styles/theme"
 import { SidebarProvider } from "../contexts/SidebarContext"
+import { makeServer } from "../services"
+
+if(process.env.NODE_ENV ==="development"){
+    makeServer();
+}
+
 function MyApp({ Component, pageProps}:AppProps) {
   return (
   <ChakraProvider  theme={theme}>
